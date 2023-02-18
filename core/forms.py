32d03ce878e2945f71c4898538ed570074c851e1
Troxed <116315838+Troxed@users.noreply.github.com>
2023-02-17 21:14:20 -0500

    Ton of styling with tailwind and some JS

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
 class UploadPhotosForm(forms.Form):
-    person_photo = forms.ImageField(label='Upload a photo of the person')
-    group_photo = forms.ImageField(label='Upload a photo of the group')
+    person_photo = forms.ImageField(
+        label='Face to search for',
+        widget=forms.ClearableFileInput(attrs={'onchange': 'preview_image(event, "person-preview")'})
+    )
+    group_photo = forms.ImageField(
+        label='Photo to search in',
+        widget=forms.ClearableFileInput(attrs={'onchange': 'preview_image(event, "group-preview")'})
+    )
+
 
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from .forms import UploadPhotosForm
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import face_recognition
 import base64
 import io
@@ -35,17 +35,46 @@ class UploadPhotosView(FormView):
         # Check for matches and draw boxes
         pil_image = Image.fromarray(group_photo)
         draw = ImageDraw.Draw(pil_image)
+        width, height = pil_image.size
 
+        match_count = 0
         for (top, right, bottom, left), face_encoding, landmarks in zip(group_face_locations, group_face_encodings,
                                                                         group_face_landmarks):
             matches = face_recognition.compare_faces([person_face_encoding], face_encoding, tolerance=.48)
 
             # If match
             if True in matches:
+                match_count += 1
                 first_match_index = matches.index(True)
 
                 # Draw box around face
-                draw.rectangle(((left - 5, top - 5), (right + 5, bottom + 5)), outline=(0, 255, 0), width=3)
+                draw.rectangle(((left - 5, top - 5), (right + 5, bottom + 5)), outline=(0, 255, 0), width=4)
+
+                # Get the confidence level and print it below the box
+                confidence = face_recognition.face_distance([person_face_encoding], face_encoding)[first_match_index]
+                confidence_percentage = round((1 - confidence) * 100, 2)
+                draw.text((left, bottom + 10), f"Confidence: {confidence_percentage}%", fill=(0, 255, 0),
+                          font=ImageFont.truetype("arial.ttf", 28))
+
+        if match_count < 1:
+            # Write "No matches found" on image
+            font_ratio = 1 / 10
+            font_size = int(min(pil_image.size) * font_ratio)
+            font = ImageFont.truetype("arial.ttf", font_size)
+            color = (255, 0, 0, 255)
+            message = "No matches found"
+            text_image = Image.new('RGBA', (width, height), (255, 255, 255, 120))
+            text_draw = ImageDraw.Draw(text_image)
+            text_width, text_height = text_draw.textsize(message, font)
+            x = (width - text_width) / 2
+            y = (height - text_height) * .9
+            text_draw.text((x, y), message, font=font, fill=color)
+            pil_image = pil_image.convert('RGBA')
+            pil_image = Image.alpha_composite(pil_image, text_image)
+
+
+
+
 
         # Save the image to a buffer
         buf = io.BytesIO()
