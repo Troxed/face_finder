40d4412644d53e9c4a2a3b6a8e7452b5ef5ddeae
Troxed <116315838+Troxed@users.noreply.github.com>
2023-03-02 18:30:48 -0500

    dockerfile changes

--- staticfiles/js/preview.js
+++
@@ -1,9 +0,0 @@
-function preview_image(event, preview_id) {
-    var reader = new FileReader();
-    reader.onload = function() {
-        var output = document.getElementById(preview_id);
-        output.src = reader.result;
-        output.style.display = "block";
-    }
-    reader.readAsDataURL(event.target.files[0]);
-}
