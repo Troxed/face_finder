e0d8c7fd3a8deac0e93ae1986fc91985bee6def9
Troxed <116315838+Troxed@users.noreply.github.com>
2023-03-21 11:16:30 -0400

    reconfigured CSRF_TRUSTED_ORIGINS

--- face_finder/settings.py
+++ face_finder/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS=["*"]
+CSRF_TRUSTED_ORIGINS=['https://*.facefinder.up.railway.app', 'https://*.facefinderapp.com', '127.0.0.1:8000']
 
 # Application definition
 
